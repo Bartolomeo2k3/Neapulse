ade238ee5ade71fc2e5901ebd335540546bed1ac
feerdd <148528966+feerdd@users.noreply.github.com>
2023-10-30 20:46:30 +0100

    h

--- Neapulse/CCharacter/HistoryView.swift
+++ Neapulse/CCharacter/HistoryView.swift
@@ -8,28 +8,11 @@
 import SwiftUI
 
 struct HistoryView: View {
-    
-    var character: Character
-    
     var body: some View {
-            VStack {
-                Image(learner.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    .frame(width: 250)
-                    .padding()
-                Text("\(learner.name) \(learner.surname)")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundStyle(learner.favouriteColour)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                Text(learner.description)
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {let character = Character(name: "Filippo",  description: "Some description", image: "filippo")
+#Preview {
     HistoryView()
 }
