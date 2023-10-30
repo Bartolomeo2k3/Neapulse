cf2ab64613c6e2543186a980571d0edc38c5f121
vitorgebrin <vitorgebrin@gmail.com>
2023-10-30 23:01:10 +0100

    Quiz_part_1

--- Neapulse/New Group/QuizView.swift
+++ Neapulse/New Group/QuizView.swift
@@ -10,20 +10,39 @@ import SwiftUI
 struct QuizView: View {
     @State var nostroQuiz = Quiz(questions: [Question(text: "Sei Bart?",answers: ["Sì","No"],charIndex: [[0,1],[]]),Question(text: "Sei Vitor?",answers: ["Sì","No"],charIndex: [[0],[]])])
     var body: some View {
-        Text(nostroQuiz.getQuestion())
+        VStack(spacing:35){
+            
+            ProgressView(value: Float(nostroQuiz.questionIndex)/Float(nostroQuiz.questions.count))
+                .progressViewStyle(.linear)
+                .padding()
+        HStack(alignment:.center){
+            Text(nostroQuiz.getQuestion()).font(.title)
+                .multilineTextAlignment(.center)
+                .bold()
+        }.padding()
         Button(nostroQuiz.getAnswers()[0]){
             nostroQuiz.increasePoints(index:nostroQuiz.getCharacters(_var: 0))
             nostroQuiz.nextQuestion()
-        }
+        }.font(.title3)
+            .foregroundColor(.black)
+            .frame(width:300,height: 55)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius:5.0)
+        
         Button(nostroQuiz.getAnswers()[1]){
             nostroQuiz.increasePoints(index:nostroQuiz.getCharacters(_var: 1))
             nostroQuiz.nextQuestion()
-        }
-        
-        Text(String(nostroQuiz.characters[0].points))
-        Text(String(nostroQuiz.characters[1].points))
-    }
+        }.font(.title3)
+            .foregroundColor(.black)
+            .frame(width:300,height: 55)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius:5.0)
     
+    
+        }.frame(height:670)
+    }
 }
 
 #Preview {
