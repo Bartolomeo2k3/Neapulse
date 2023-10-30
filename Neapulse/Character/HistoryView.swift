ee27538654980aec3e82fa0e3baa6aa8c7a080cf
feerdd <148528966+feerdd@users.noreply.github.com>
2023-10-30 23:44:30 +0100

    iuy

--- Neapulse/Character/HistoryView.swift
+++ Neapulse/Character/HistoryView.swift
@@ -15,8 +15,8 @@ struct HistoryView: View {
         ScrollView{
             Image(charImage).resizable()
                 .aspectRatio(contentMode: .fit).shadow(color: .gray, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text(charName).fontWeight(.semibold)
-            Text(charText).padding(40).foregroundColor(.gray)
+            Text(charName).fontWeight(.bold)
+            Text(charText).padding(25).foregroundColor(.gray).multilineTextAlignment(.center)
         }}
 }
 
