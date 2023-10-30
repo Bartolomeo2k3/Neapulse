cd9f720943885094da444dd590afd7200c9ea4a2
Bartolomeo2k3 <75879635+Bartolomeo2k3@users.noreply.github.com>
2023-10-30 10:40:09 +0100

    k,mkjkmkm

---
+++ Neapulse/AllowLocation2.swift
@@ -0,0 +1,24 @@
+
+//
+//  AllowLocation.swift
+//  Neapulse
+//
+//  Created by Bartolomeo Davide Ventrone on 28/10/23.
+//
+
+import SwiftUI
+
+struct AllowLocation: View {
+    @ObservedObject var locationManager = LocationManager.shared
+    var body: some View {
+        if locationManager.userLocations == nil{
+            MapView()
+        }else{
+            Text("Location not Allowed")
+        }
+    }
+}
+
+#Preview {
+    AllowLocation()
+}
